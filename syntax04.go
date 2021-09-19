724b80c6a2a3c56be48544190d0befdd51470c52
king4474 <king4474@naver.com>
2021-09-19 17:39:23 +0900

    v0.6

--- syntax04.go
+++ syntax04.go
@@ -1,3 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// 난수 추출된 수의 소수 판정 프로그램 v0.6
+func main() {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+
+	isPrime := true
+	number := rand.Intn(150) + 2
+	fmt.Println("임의로 추출된 수 : ", number)
+
+	for i := 2; i < number; i++ {
+		if number%i == 0 {
+			isPrime = false
+			break
+		}
+		fmt.Print(i, " ")
+	}
+
+	if isPrime { // 비교 연산 제거
+		fmt.Println(number, "는(은) 소수입니다.")
+	} else {
+		fmt.Println(number, "는(은) 소수가 아닙니다.")
+	}
+}
 
 // package main
 
