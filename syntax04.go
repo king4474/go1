ebae1bd751347edf036a5658b816db4761cb8b19
king4474 <king4474@naver.com>
2021-09-19 17:38:27 +0900

    v0.3

--- syntax04.go
+++ syntax04.go
@@ -7,6 +7,37 @@
 // 	"time" // seed 생성용 패키지
 // )
 
+// // 난수 추출된 수의 소수 판정 프로그램 v0.3
+// func main() {
+// 	seed := time.Now().Unix()
+// 	rand.Seed(seed)
+
+// 	isPrime := true
+// 	number := rand.Intn(150) + 2
+// 	fmt.Println("임의로 추출된 수 : ", number)
+
+// 	for i := 2; i < number; i++ {
+// 		if number%i == 0 {
+// 			//count++
+// 			isPrime = false
+// 		}
+// 	}
+
+// 	if isPrime == true {
+// 		fmt.Println(number, "는(은) 소수입니다.")
+// 	} else {
+// 		fmt.Println(number, "는(은) 소수가 아닙니다.")
+// 	}
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time" // seed 생성용 패키지
+// )
+
 // // 난수 추출된 수의 소수 판정 프로그램 v0.2
 // func main() {
 // 	seed := time.Now().Unix()
